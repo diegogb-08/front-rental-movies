84ffbc2aba109d54f007007e6e026d6aeca87422
Diego <d.garciabrisa@gmail.com>
2021-03-24 13:30:41 +0100

    adding variable for scss

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -4,7 +4,7 @@ import ModalRender from "../Modal/ModalRender";
 const Home = () => {
     return (
         <div className="homeContainer">
-
+            <div className="nesting"></div>
             Hola soy home
             <ModalRender name="Tenemos Modal"></ModalRender>
             <button className="btn btn-primary"></button>
