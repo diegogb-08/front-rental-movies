e3339e44ff97ad682b3ef3ff3a9a2068c2c42765
Roo-Git <robert.martinez.delafuente@gmail.com>
2021-03-24 17:59:45 +0100

    Added register component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Home from './containers/Home/Home';
+import Register from './components/Register/Register';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
 
       <Switch>
         <Route path="/" exact component={Home}/>
+        <Route path="/register" exact component={Register}/>
 
       </Switch>
       </BrowserRouter>
