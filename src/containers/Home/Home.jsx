5b6bcedd1eb6656c65f10007386f0668b0d3924d
vicGeo <vicsanig@alumni.upv.es>
2021-03-24 20:25:07 +0100

    added section home

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,15 +1,41 @@
-import React from "react";
-import ModalRender from "../Modal/ModalRender";
+import React from 'react'
 
-const Home = () => {
+function Home(props) {
+
     return (
         <div className="homeContainer">
-            <div className="nesting"></div>
-            Hola soy home
-            <ModalRender name="Tenemos Modal"></ModalRender>
-            <button className="btn btn-primary"></button>
+
+            <div className="sectionHome">
+
+            </div>
+
+            <div className="sectionWatch">
+            <div className="containerWatchText">
+            <h1>Enjoy on your TV.</h1>
+            <h2>Watch on Smart TVs, Playstation, Xbox<br></br> 
+                Chromecast, Apple TV, Blu-ray players, and<br></br>
+                more.</h2>
+            </div>
+
+            </div>
+
+            <div className="sectionDownload">
+
+            </div>
+
+            <div className="sectionDevice">
+
+            </div>
+
+            <div className="sectionQuestions">
+
+            </div>
+
+
+
+
         </div>
     )
-};
+}
 
-export default Home;
+export default Home
