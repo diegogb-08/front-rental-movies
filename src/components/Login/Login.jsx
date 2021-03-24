71f94184651db5c8eb7ea2643f0d8aa8534fa6f3
Diego <d.garciabrisa@gmail.com>
2021-03-24 20:00:37 +0100

    button added to login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Button from '../Button/Button'
+import InputForm from '../InputForm/InputForm'
 
 function Login(props) {
 
+    const [login, setLogin] = useState({
+        email: '',
+        password: ''
+    })
+
+    console.log(login)
+
+    const handleState = (e) => {
+        setLogin({...login, [e.target.name]: e.target.type === "number" ? + e.target.value : e.target.value});
+    }
+
     return (
         <div className="loginComponent">
-            
+            <div className="loginBox">
+                <div className="inputLogin">
+                    <InputForm type="text" name="email" onChange={handleState} title="Email"/>
+                </div>
+                <div className="inputLogin">        
+                    <InputForm type="password" name="password" onChange={handleState} title="Password"/>
+                </div>
+                <div className="inputLogin">
+                    <Button name="Sign In" />
+                </div>
+            </div>
         </div>
     )
 }
