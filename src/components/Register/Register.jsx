fade025ce5daf5102660dfad6b8a02b8f9440855
Roo-Git <robert.martinez.delafuente@gmail.com>
2021-03-25 13:36:10 +0100

    Added HeaderRegister component

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,38 +1,20 @@
 import React from "react";
-import { useHistory } from "react-router-dom";
+
 
 // IMPORT COMPONENTS
+import HeaderRegister from '../HeaderRegister/HeaderRegister';
 import Button from '../../components/Button/Button';
 import InputForm from '../../components/InputForm/InputForm';
 
-// IMPORT IMG
-import logo from "../../img/logo.png";
+
 
 
 
 const Register = (props) => {
 
-    let history = useHistory();
-
-    function handleClick() {
-        history.push("/");
-    };
-
-    function handleClickLogin() {
-        history.push("/login");
-    };
-
-
-
     return (
-        <div className="registerContainer">
-            <div className="headerReg">
-
-                <div className="fakeFlixContainer"><img src={logo} alt="" type="button" onClick={handleClick}/></div>
-                <div className="buttonReg" type="button" onClick={handleClickLogin}>Sign in</div>    
-    
-            </div>
-
+        <div className="registerContainer">            
+                <HeaderRegister/>            
             <div className="superformReg">
                 <div className="formReg">
                     <div className="errorDivReg">
