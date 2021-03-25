caf2ce7f4d2bd5f7b5eddb35e7440874aaab063d
Roo-Git <robert.martinez.delafuente@gmail.com>
2021-03-25 14:24:18 +0100

    Added FirstStepRegister Component

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -4,8 +4,7 @@ import React from "react";
 // IMPORT COMPONENTS
 import HeaderRegister from '../HeaderRegister/HeaderRegister';
 import ErrorRegister from '../ErrorRegister/ErrorRegister';     
-import Button from '../../components/Button/Button';
-import InputForm from '../../components/InputForm/InputForm';
+import FirstStepRegister from "../FirstStepRegsiter/FirstStepRegister";
 
 
 
@@ -17,18 +16,8 @@ const Register = (props) => {
         <div className="registerContainer">            
                 <HeaderRegister/>            
             <div className="superformReg">
-                <div className="formReg">
                     <ErrorRegister/>
-                    <p className="pFormRegUp">Welcome back geeks!</p>
-                    <p className="pFormRegUp">Joining Fakeflix is easy.</p>
-                    <p className="pFormRegMid">Enter your password and you'll be watching in no time.</p>
-                    <p className="pFormRegMid">Email
-                        <div className="propsEmailReg">user@gmail.com</div>
-                    </p>                   
-                        <InputForm type="password" name="password" title="Password"/>
-                        <div className="buttonFormReg"><Button name="Continue"/></div>                    
-                    
-                </div>
+                    <FirstStepRegister/>
 
             </div>
 
