bedd6285a2a2818fb2b8752ac0ffea30bde37434
Roo-Git <robert.martinez.delafuente@gmail.com>
2021-03-29 13:05:36 +0200

    Added TabNav n Tab to Rental view

--- src/components/Tab/Tab.jsx
+++ src/components/Tab/Tab.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './Tab.css'
+
 
 const Tab = (props) => {
 
--- src/containers/Rental/Rental.jsx
+++ src/containers/Rental/Rental.jsx
@@ -1,16 +1,50 @@
-import React from "react";
+import React, {useState} from "react";
 
 import Header from '../../components/Header/Header';
+import Tab from '../../components/Tab/Tab';
+import TabNav from "../../components/Tab/TabNav";
 
 
 
 function Rental () {
+
+  // HOOKS
+
+  const [tab, setTab] = useState({
+    selected: 'Rental Menu'
+  })
+
+ // FUNCTIONS
+
+  const setSelected = (tab) => {
+    setTab({selected: tab});
+  }
     return (
         <div className="rentalContainer">
-          <Header/>
+            <Header/>
+          
           <div className="rentalSuperMainContainer">
-            <div className="rentalMainContainer">
-              
+              <div className="rentalMainContainer">
+          <TabNav tabs={['Orders', 'Wishes', 'Last Orders', 'Gifts', 'Give Away']} selected={tab.selected} setSelected={setSelected}>
+              <Tab isSelected={tab.selected === 'Rental Menu'}>
+                <div className="cardInfoRental">
+
+
+
+
+
+
+                </div>
+
+
+
+              </Tab>
+
+
+
+
+          </TabNav>
+
             </div>
 
           </div>
