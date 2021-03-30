70d5797fd509462c5d7a0a365e331b8f6f4e4277
Roo-Git <robert.martinez.delafuente@gmail.com>
2021-03-30 13:47:11 +0200

    Added basket on Rental view

--- src/containers/Rental/Rental.jsx
+++ src/containers/Rental/Rental.jsx
@@ -8,6 +8,7 @@ import Orders from "../../components/Orders/Orders";
 import Tab from '../../components/Tab/Tab';
 import TabNav from "../../components/Tab/TabNav";
 import {connect} from 'react-redux';
+import Button from '../../components/Button/Button';
 
 
 
@@ -48,12 +49,19 @@ function Rental (props) {
           </div>
           
           <div className="basketRental">
-
-            <div>
-            <FontAwesomeIcon className='filmIconRental' icon={faFilm} />
+            <div className="iconCounterContainer">
+              <div><FontAwesomeIcon className='filmIconRental' icon={faFilm}/></div>
+              <div className="counterCartRental">{props.cart.length}</div>
             </div>
-          </div>
+            <div className="priceButtonContainer">
+                <p className="pTotalPrice">Total Price: PROPS</p>
+                <div className="rentalButton">
+                  <Button name="Buy"/>
+                </div>
+            </div>
           
+            
+          </div>
           
 
         </div>
