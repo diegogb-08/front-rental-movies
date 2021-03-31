e6977160f3dc4421f87e88ed8f9e607b0643627c
vicGeo <vicsanig@alumni.upv.es>
2021-03-31 19:23:07 +0200

    added function buyOrder

--- src/containers/Rental/Rental.jsx
+++ src/containers/Rental/Rental.jsx
@@ -1,20 +1,19 @@
-import React, {useState, useEffect} from "react";
-
+import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFilm} from '@fortawesome/free-solid-svg-icons'
-
+import { faFilm } from '@fortawesome/free-solid-svg-icons'
+import { port, customer, rental} from '../../api/ApiMongoDB';
 import Header from '../../components/Header/Header';
 import Orders from "../../components/Orders/Orders";
 import Tab from '../../components/Tab/Tab';
 import TabNav from "../../components/Tab/TabNav";
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import Button from '../../components/Button/Button';
 import { CLEAN } from "../../redux/types/cartType";
 
 
 
 
-function Rental (props) {
+function Rental(props) {
   // HOOKS
 
   const [totalPrice, setTotalPrice] = useState(0)
@@ -25,93 +24,97 @@ function Rental (props) {
 
 
   useEffect(() => {
-      calculateTotal();
-    },[]);
-  
-    
-  useEffect(()=> {
-      calculateTotal();
+    calculateTotal();
+  }, []);
+
+
+  useEffect(() => {
+    calculateTotal();
   });
-  
 
 
- // FUNCTIONS
 
-   // Delete all orders (Roo says: Molaría que fuese solo una, y no todas. JOJOOOOOOOOOOOOOOOOOOOOJO)
-   const deleteOrder = () => {
+  // FUNCTIONS
 
-    props.dispatch({type: CLEAN, payload: [] }); 
-    
-  }; 
+  // Delete all orders (Roo says: Molaría que fuese solo una, y no todas. JOJOOOOOOOOOOOOOOOOOOOOJO)
+  const deleteOrder = () => {
+
+    props.dispatch({ type: CLEAN, payload: [] });
+
+  };
+
+  const buyOrder = () => {
+
+  }
 
   const calculateTotal = () => {
-      let totalPriceOnCart = 0 
+    let totalPriceOnCart = 0
 
-      let priceArray = props.cart.map(x=> {
-        return x.price
-      })    
+    let priceArray = props.cart.map(x => {
+      return x.price
+    })
 
-      priceArray.map(num => {
-        totalPriceOnCart +=num
-      })
-      
-      setTotalPrice(totalPriceOnCart)
-      
+    priceArray.map(num => {
+      totalPriceOnCart += num
+    })
+
+    setTotalPrice(totalPriceOnCart)
+
 
 
   };
 
   const setSelected = (tab) => {
-    setTab({selected: tab});
+    setTab({ selected: tab });
   }
-    return (
-        <div className="rentalContainer">
-            <Header/>
-          
-          <div className="rentalSuperContainer">
-              
-            <TabNav tabs={['Orders', 'Wishes', 'Last Orders', 'Gifts']} selected={tab.selected} setSelected={setSelected}>
-                <Tab isSelected={tab.selected === 'Orders'}>
-                    <Orders/>
-                </Tab>
-                <Tab isSelected={tab.selected === 'Last Orders'}>
+  return (
+    <div className="rentalContainer">
+      <Header />
 
-                </Tab>
+      <div className="rentalSuperContainer">
 
-            </TabNav>
-           
-          </div>
-          
-          <div className="basketRental">
-            <div className="iconCounterContainer">
-              <div><FontAwesomeIcon className='filmIconRental' icon={faFilm}/></div>
-              <div className="counterCartRental">{props.cart.length}</div> 
-            </div>
-            <div className="priceButtonContainer">
-                <p className="pTotalPrice">{totalPrice}€</p>
-            </div>
-            <div className="containerButtonsRental">
-              <div className="buyButton">
-                <Button name="Buy"/>
-              </div>
-              <div className="emptyOrdersButton">
-                <Button onClick={()=> deleteOrder()} name="Empty Orders"/>
-              </div>
-            </div>
+        <TabNav tabs={['Orders', 'Wishes', 'Last Orders', 'Gifts']} selected={tab.selected} setSelected={setSelected}>
+          <Tab isSelected={tab.selected === 'Orders'}>
+            <Orders />
+          </Tab>
+          <Tab isSelected={tab.selected === 'Last Orders'}>
 
-                  
+          </Tab>
 
-                     
-          </div>
-          
+        </TabNav>
+
+      </div>
+
+      <div className="basketRental">
+        <div className="iconCounterContainer">
+          <div><FontAwesomeIcon className='filmIconRental' icon={faFilm} /></div>
+          <div className="counterCartRental">{props.cart.length}</div>
         </div>
-    )
+        <div className="priceButtonContainer">
+          <p className="pTotalPrice">{totalPrice}€</p>
+        </div>
+        <div className="containerButtonsRental">
+          <div className="buyButton">
+            <Button name="Buy" onClick={() => buyOrder()}/>
+          </div>
+          <div className="emptyOrdersButton">
+            <Button onClick={() => deleteOrder()} name="Empty Orders" />
+          </div>
+        </div>
+
+
+
+
+      </div>
+
+    </div>
+  )
 };
 
 const mapStateToProps = state => {
   return {
-      cart : state.cartReducer.cart,
-      
+    cart: state.cartReducer.cart,
+
   }
 }
 
--- src/redux/reducers/cartReducer.js
+++ src/redux/reducers/cartReducer.js
@@ -1,57 +1,57 @@
-import {ADD, REMOVE, CLEAN, EDIT, TOTAL_CART} from '../types/cartType';
+import { ADD, REMOVE, CLEAN, EDIT, TOTAL_CART } from '../types/cartType';
 
 const initialState = {
-    cart : [],
-    totalCart : 0
+    cart: [],
+    totalCart: 0
 };
 
 const cartReducer = (state = initialState, action) => {
-    switch(action.type){
-        case ADD :
+    switch (action.type) {
+        case ADD:
             return {
                 ...state,
 
                 cart: [...state.cart, action.payload]
             };
-        
-        case REMOVE : 
+
+        case REMOVE:
             return {
                 ...state,
-                cart : state.cart.filter(cart => cart.id !== action.payload)
+                cart: state.cart.filter(cart => cart.id !== action.payload)
             };
 
-        case CLEAN : 
+        case CLEAN:
             return {
                 ...state,
-                cart : action.payload
+                cart: action.payload
             };
-        
-           
-        case EDIT:  
-		
-            let newCart = state.cart.map( (_x) => {
-                
+
+
+        case EDIT:
+
+            let newCart = state.cart.map((_x) => {
+
                 if (_x.price === action.payload.price) { // If it already exists
                     _x.onCart = action.payload.newAmount; // I modity it
                 };
-                
-                return _x;
-                
-            });
-        
-          return {
-            ...state,
-            cart: newCart,
 
-          };
-          
-        case TOTAL_CART :
+                return _x;
+
+            });
+
             return {
-              ...state,
-              totalCart : action.payload
+                ...state,
+                cart: newCart,
+
             };
 
-        default : 
+        case TOTAL_CART:
+            return {
+                ...state,
+                totalCart: action.payload
+            };
+
+        default:
             return state
     }
 }
