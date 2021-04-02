dc68ba5470b189c2995d8058a254707f8a05de25
Roo-Git <robert.martinez.delafuente@gmail.com>
2021-04-02 19:36:37 +0200

    Refactoring orders component style

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -37,7 +37,7 @@ function Orders(props) {
                     <>
                     <div className="emptyOrderContainer">
                       <div className="messageEmpyOrder">
-                        <p className="pEmptyOrder">Waiting for your orders</p>                
+                        <p className="pEmptyOrder">Waiting for your orders.</p>                
                       </div>
                       <div className="containerCat">
                         <img className="sadCat" src={rental} alt="sadCat"/>
